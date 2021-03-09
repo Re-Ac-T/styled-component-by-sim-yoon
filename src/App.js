865d5279bd15e74bffad03b253bfdc44866f13aa
merorii <hsoneee@naver.com>
2021-03-09 23:40:08 +0900

    Add movieList feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MovieList from './components/MovieList';
 
 function App() {
+  const [state, setState] = useState({
+    state: false,
+    movie: {}
+  });
+  
   return (
-    <div className="App">
-      <h1> Project #1 Styled Component </h1>
-      <div>
-
-      </div>
-    </div>
+    <>
+      <MovieList setState={setState} />
+      {state.state && console.log(state.movie)}
+      {/* 바로 위 콘솔 지우시고 콘솔부분에 희구님 컴포넌트이름 넣어서 아래처럼 사용하시면 됩니당! */}
+      {/* <컴포넌트이름 movie={state.movie} /> */}
+    </>
   );
 }
 

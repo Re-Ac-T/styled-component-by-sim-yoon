59e3d0ec3211e725f047e982ac783c3ee80c7f6a
heegu0311 <heegu0311@naver.com>
2021-03-11 23:58:13 +0900

    Add PopUp feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import MovieList from './components/MovieList';
+import PopUp from './components/PopUp';
 
 function App() {
   const [state, setState] = useState({
@@ -9,8 +10,7 @@ function App() {
   
   return (
     <>
-      <MovieList setState={setState} />
-      {state.state && console.log(state.movie)}
+      {state.state ? <PopUp movie={state.movie} setState={setState}/> : <MovieList setState={setState} />}
       {/* 바로 위 콘솔 지우시고 콘솔부분에 희구님 컴포넌트이름 넣어서 아래처럼 사용하시면 됩니당! */}
       {/* <컴포넌트이름 movie={state.movie} /> */}
     </>
